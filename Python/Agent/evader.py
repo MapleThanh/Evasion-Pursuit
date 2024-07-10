9e8a72f9f092621bdc79c517a66632896d13df39
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2024-07-11 00:39:03 +0700

    Create two classes for evader and pursuer

---
+++ Python/Agent/evader.py
@@ -0,0 +1,19 @@
+from agent import Agent
+
+class Evader(Agent):
+    def __init__(self, x, y, max_speed, acceleration, strategy):
+        super().__init__(x, y, max_speed, acceleration)
+        self.pursuers = []
+        self.strategy = strategy
+
+    def add_pursuer(self, pursuer):
+        if pursuer not in self.pursuers:
+            self.pursuers.append(pursuer)
+
+    def remove_pursuer(self, pursuer):
+        if pursuer in self.pursuers:
+            self.pursuers.remove(pursuer)
+
+    def evade(self, boundaries):
+        desired_direction = self.strategy.calculate_move(self, self.pursuers, boundaries)
+        self.move(desired_direction)
---
+++ Python/Agent/pursuer.py
@@ -0,0 +1,16 @@
+from agent import Agent
+
+class Pursuer(Agent):
+    def __init__(self, x, y, max_speed, acceleration, strategy):
+        super().__init__(x, y, max_speed, acceleration)
+        self.evader = None
+        self.strategy = strategy
+
+    def set_evader(self, evader):
+        self.evader = evader
+
+    def pursue(self, boundaries):
+        if self.evader is None:
+            return
+        desired_direction = self.strategy.calculate_move(self, self.evader, boundaries)
+        self.move(desired_direction)
